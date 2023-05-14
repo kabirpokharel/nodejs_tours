3799c5dc3230210a677dbbfe5df23449c9841112
kabir pokharel <kabirpokharel12@gmail.com>
2023-05-14 23:40:46 +0930

    route refactored

--- app.js
+++ app.js
@@ -140,9 +140,6 @@ const createTour = (req, res) => {
 
 //3)Routes
 
-app.use('api/v1/tours', tourRouter);
-app.use('api/v1/users', tourRouter);
-
 const tourRouter = express.Router();
 const userRouter = express.Router();
 
@@ -150,14 +147,12 @@ tourRouter.route('/').get(getAllTours).post(createTour);
 
 tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-userRouter.route('/api/v1/users').get(getAllUsers).post(createUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
 
-userRouter
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
+app.use('api/v1/tours', tourRouter);
+app.use('api/v1/users', userRouter);
 //4)Start server
 const PORT = 3000;
 app.listen(PORT, () => {
